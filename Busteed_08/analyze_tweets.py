987a161e74ed54c1b8871faf1d3cffffb2626cb7
dbusteed <busteed.davis@gmail.com>
2019-03-16 11:25:55 -0600

    done with HW #8

--- Busteed_08/analyze_tweets.py
+++ Busteed_08/analyze_tweets.py
@@ -1,13 +1,18 @@
+# Davis Busteed -- LING 360 -- HW 8
+
+# import modules
 import re
 import json
 import statistics as st
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from nltk.corpus import stopwords
-from get_tweets import MAIN_HASHTAG
+from get_tweets import MAIN_HASHTAG # get_tweets.py located in same directory
 
+# define constants
 SUBTOPIC = 'galaxy'
 STOPWORDS = stopwords.words('english')
 
+# function for processing the tweets
 def process_tweet(key, text):
 
     # add the sentiment compound score
@@ -20,9 +25,12 @@ def process_tweet(key, text):
         else:
             res[key]['neg_tweets'] += text + ' '
 
-
+# define the Sentiment analyzer object
 sia = SentimentIntensityAnalyzer()
 
+# dictionary for keeping track ofc counts / sentiments
+# 'main' refers to the main hashtag (the company)
+# 'sub' refers to the subtopic (a product of the company)
 res = {
     'main': { 
         'hash': MAIN_HASHTAG, 
@@ -38,7 +46,7 @@ res = {
     },
 }
 
-# loop thru the JSON strings 
+# loop thru the JSON strings and process tweets
 with open(f'{MAIN_HASHTAG}_tweets.json', 'r', encoding='utf8') as f:
     for raw_tweet in f:
 
@@ -52,32 +60,54 @@ with open(f'{MAIN_HASHTAG}_tweets.json', 'r', encoding='utf8') as f:
         # process all the tweets that had the main hashtag (all strings in the JSON)
         process_tweet('main', text)
 
+# find top 5 words for both pos_tweets and neg_tweets
 for sent in ['pos', 'neg']:
+
+    # grab the tweets, 's' --> one big string tweets
+    # (see process_tweet function)
     s = res['sub'][f'{sent}_tweets'].lower()
     
+    # get rid of links, #hashtags, @usernames, and 'RT' indicators
+    # also remove instances of the company hashtag and product
+    # I'm aware the next two line can be combinded into one, but this is more readable
     s = re.sub(r'(https?[^\s]*)|(#\w+)|(@\w+)|(rt)', '', s)
     s = re.sub(rf'{MAIN_HASHTAG}|{SUBTOPIC}', '', s)
     
+    # split the string of tweets into words on punctuation marks, symbols, and quotes
     tokens = re.split(r'[&#%\d:"\]\[\u201c\u201d\s+\.\?!,)(/-]', s)
+
+    # remove all tokesn that are in the STOPWORDS or are 1 char (also gets rid of '')
     tokens = [t for t in tokens if t not in STOPWORDS and len(t) > 1]
 
+    # loop thru each token
     for word in tokens:
+
+        # count each instance of a word
         if word not in res['sub'][f'{sent}_most']:
             res['sub'][f'{sent}_most'][word] = 1
         else:
             res['sub'][f'{sent}_most'][word] += 1
 
+    # replace the word freq dictionary with a sorted list of tuples (only top 5 tho, notice the [:5])
     res['sub'][f'{sent}_most'] = sorted(res['sub'][f'{sent}_most'].items(), key=lambda x:x[1], reverse=True)[:5]
 
-# output the results
-print('\nSentiment comparisons')
+# output time!
+print('\nSentiment comparisons\n-------------')
 for k in res.keys():
-    res[k]['total_sent'] = st.mean( res[k]['sent_scores'] )
-    print(f'Compound sentiment for {res[k]["hash"]}: {res[k]["total_sent"]:05.3f}')
 
-# 
+    # calculate the average sentiment and output 
+    res[k]['total_sent'] = st.mean( res[k]['sent_scores'] )
+    print(f'Compound sentiment for "{res[k]["hash"]}": {res[k]["total_sent"]:05.3f}')
+
+# dipsplay which was greater
+if res['main']['total_sent'] > res['sub']['total_sent']:
+    print(f'\nSentiment for "{res["main"]["hash"]}" (the company) is greater than "{res["sub"]["hash"]}" ' )
+else:
+    print(f'\nSentiment for "{res["sub"]["hash"]}" (the product) is greater than "{res["main"]["hash"]}" ' )
+
+# show the top 5 words for positive and negative tweets
 for sent in ['pos', 'neg']:
-    print(f'\nTop 5 words in {sent}-ative tweets')
+    print(f'\nTop 5 words for {sent} tweets\n-------------')
     for (w, _) in res['sub'][f'{sent}_most']:
         print(w)
 
--- Busteed_08/get_tweets.py
+++ Busteed_08/get_tweets.py
@@ -2,36 +2,43 @@
 
 # import modules
 import api_config  # api_config.py located in working directory,
-import tweepy      # add your own api_config or hardcode tokens to run script
-import json
+import tweepy      # add your keys to api_config or hardcode tokens to run script
+import json        # if hardcoding the keys, then remove 'import api_config'
 
+# the main hashtag / topic to look for
 MAIN_HASHTAG = 'samsung'
 TWEET_COUNT = 1000
 
+# main() / if __name__ stuff at the bottom allows me to 
+# import the MAIN_HASHTAG constant from above into the 
+# other scripts, without running the code below
 def main():
     
+    # add your keys here!
     consumer_key = api_config.consumer_key
     consumer_secret_key = api_config.consumer_secret_key
-    # consumer_key = YOUR_KEY_HERE
-    # consumer_secret_key = YOUR_KEY_HERE
 
     access_token = api_config.access_token
     access_token_secret = api_config.access_token_secret
-    # access_token = YOUR_KEY_HERE
-    # access_token_secret = YOUR_KEY_HERE
 
+    # authenticate thru Twitter with the keys
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret_key)
     auth.set_access_token(access_token, access_token_secret)
 
+    # connect to the API
     api = tweepy.API(auth, wait_on_rate_limit=True)
 
+    # go get the tweets! get 280 char english tweets with our hashtag of choice
     tweets = tweepy.Cursor(api.search, q=f'#{MAIN_HASHTAG}', tweet_mode='extended', lang='en').items(TWEET_COUNT)
 
-    # save our tweets!
+    # save our tweets! note: this isn't a 'real' json file, so after
+    # creating it, your IDE may show errors. it is purposefully formatted
+    # as a file of JSON strings
     with open(f'{MAIN_HASHTAG}_tweets.json', 'w', encoding='utf8') as f:
         for t in tweets:
             f.write(json.dumps(t._json, ensure_ascii=True))
             f.write('\n')
 
+# this is explained above
 if __name__ == '__main__':
     main()
